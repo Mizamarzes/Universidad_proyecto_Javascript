20249376e77bf4a3a341c69d9a66279da1cb5193
mizamarzes <mizamarzes@gmail.com>
2024-03-01 17:25:12 -0500

    config function saveJson()

--- js/utils.js
+++ js/utils.js
@@ -1,37 +1,21 @@
 const periodsList=[];
+const url_period="students"
 
 const loadJson=async(url,list)=>{
     try{
         list.length=0;
-        const response=await fetch(url);
+        const response=await fetch("http://localhost:3000/"+url);
 
         if(!response.ok){
             throw new Error(`Error loading, status: ${response.status}`);
         }
-        const list=await response.json();
-        list.push(...products);
+        const listJSON=await response.json();
+        list.push(...listJSON);
     }catch(error){
         console.error(`Error loading products, ${error.message}`);
     }
 }
 
-const saveJson= async(newProduct)=>{
-    try{
-        const response=await fetch('http://localhost:3000/products',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify(newProduct),
-        });
-        
-        if(!response.ok){
-            throw new Error(`Error saving product, status: ${response.status}`);
-        }
-        const productCreated= await response.json();
-        console.log(`Created product: ${productCreated}`);
+loadJson(url_period, periodsList);
+console.log(periodsList)
 
-    }catch(error){
-        console.log(`Error saving product ${error.message}`);
-    }
-}
